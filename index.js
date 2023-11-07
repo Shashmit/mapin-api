82b75ff89cac8e5864486b0575d39d7e74e061fb
Shashmit kumar <shaashmit@gmail.com>
2023-11-07 07:01:39 +0530

    to check

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const calls = require("./handlers/urlHandle");
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 // all the using tools
@@ -18,11 +18,15 @@ app.use("/calls", calls);
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
 });
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: "Internal server error" });
+});
 //Connect to db
 mongoose
   .connect(process.env.CONNECTION_TO_MONGOOSE)
   .then(() => {
-    app.listen(process.env.PORT_NUMBER || 4000, "0.0.0.0", () => {
+    app.listen(port, () => {
       console.log("YOUR SERVER IS RUNNING");
     });
   })
